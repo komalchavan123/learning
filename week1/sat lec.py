b4ed17359ca0a64bf3456b06488f7cb0ccdc6251
komalchavan123 <50177084+komalchavan123@users.noreply.github.com>
2019-05-25 18:51:04 +0530

    Add files via upload

---
+++ week1/sat lec.py
@@ -0,0 +1,60 @@
+print("komal means softness")
+print("In space, astronauts cannot cry because there is no gravity \n and tears can't flow")
+
+
+#taking input from user
+a= int(input("enter a first no"))
+b= int(input("enter a second no"))
+c= a+b
+c=a-b
+c=a*b
+c=a/b
+print(c)
+
+
+#operator using format mathod
+a=int(input("Enter value for a"))
+b=int(input("Enter value for b"))
+c=int(input("Enter value for c"))
+d=int(input("Enter value for d"))
+e=int(input("Enter value for e"))
+f=int(input("Enter value for f"))
+g=int(input("Enter value for g"))
+h=int(input("Enter value for h"))
+
+i=a+b
+j=c-d
+k=e*f
+l=g/h
+
+L=i>j
+M=k<l
+
+print("add is {},sub is {},product is {},divison is {} L is {} and M is {}".format(i,j,k,l,L,M))
+
+
+
+#calculator
+a=int(input("kindly enter 1st number"))
+b=int(input("kindly enter 2nd number"))
+c=a+b
+d=a*b
+e=a-b
+f=a/b
+
+print("Addition of two number is",c)
+print("Product of two number is",d)
+print("Substraction of two number is",e)
+print("Division of two number is",f)
+print(type(f))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+
+
+
+
+
+
